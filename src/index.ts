8daaadabbfcbad420c7056a2e5ce079c3416b9c2
Ricardo Kustner <ricardo.kustner@postnl.nl>
2022-03-24 15:42:29 +0100

    added dynamodb client call

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,38 @@
 import * as core from '@actions/core';
+import { QueryCommand } from "@aws-sdk/client-dynamodb";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 
-async function main() {
-    try {
-        core.startGroup('Starting...');
-        const input = {
-            region: core.getInput('region'),
-            table: core.getInput('table'),
-            partitionKey: core.getInput('partition-key'),
-            sortKey: core.getInput('sort-key'),
-            sortKeyAction: core.getInput('sort-key-action'),
-        }
-        console.log(input)
-    } catch (error) {
-        core.setFailed('Error!!!');
-    } finally {
-        core.endGroup();
-    }
+export const input = {
+    region: core.getInput('region'),
+    table: core.getInput('table'),
+    partitionKey: core.getInput('partition-key'),
+    sortKey: core.getInput('sort-key'),
+    partitionKeyValue: core.getInput('partition-key-value'),
+    sortKeyValue: core.getInput('sort-key-value'),
+    sortKeyAction: core.getInput('sort-key-action'),
 }
 
-main();
+export const REGION = input.region;
+export const ddbClient = new DynamoDBClient({ region: REGION });
+
+export const params = {
+    KeyConditionExpression: `${input.partitionKey} = :p and ${input.sortKey} ${input.sortKeyAction} :s`,
+    ExpressionAttributeValues: {
+      ":p": { S: input.partitionKeyValue },
+      ":s": { S: input.sortKeyValue },
+    },
+    TableName: input.table,
+};
+
+export const run = async () => {
+    try {
+        const data = await ddbClient.send(new QueryCommand(params));
+        data.Items!.forEach(function (element) {
+            console.log(element);
+          });
+        } catch (err) {
+            console.error(err);
+          }
+        };
+run();
+
