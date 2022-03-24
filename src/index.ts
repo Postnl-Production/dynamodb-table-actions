0b885461f249e1ca47ed16fc44cddf75fdfeaccd
Ricardo Kustner <ricardo.kustner@postnl.nl>
2022-03-24 13:05:13 +0100

    debug input vars

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,16 @@ import * as core from '@actions/core';
 async function main() {
     try {
         core.startGroup('Starting...');
+        const input = {
+            region: core.getInput('region'),
+            table: core.getInput('table'),
+            partitionKey: core.getInput('partition-key'),
+            sortKey: core.getInput('sort-key'),
+            sortKeyAction: core.getInput('sort-key-action'),
+        }
+        console.log(input)
     } catch (error) {
-        core.setFailed(error.message);
+        core.setFailed('Error!!!');
     } finally {
         core.endGroup();
     }
