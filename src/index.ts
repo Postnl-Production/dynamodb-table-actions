12ab23057bd1aefa9e4eb05c27c0064e39a93846
Leone Perdigão <leone.lage@gmail.com>
2022-03-28 11:13:35 +0200

    feat: set output as JSON string. Code formatting.

--- src/index.ts
+++ src/index.ts
@@ -4,41 +4,37 @@ import { ddbDocClient } from "./ddbDocClient";
 import { unmarshall } from "@aws-sdk/util-dynamodb";
 
 export const params = {
-    region: core.getInput('region'),
-    table: core.getInput('table'),
-    input: core.getInput('input'),
-    action: core.getInput('action'),
-    resultSelector: core.getInput('result-selector'),
+  region: core.getInput('region'),
+  table: core.getInput('table'),
+  input: core.getInput('input'),
+  action: core.getInput('action'),
+  resultSelector: core.getInput('result-selector'),
 }
 
 export const REGION = params.region;
 
-export const run = async () => {
-    try {
-        const data = await ddbDocClient.send(new QueryCommand({
-          ...JSON.parse(params.input),
-          TableName: params.table,
-        }));
-        const items = data.Items!.map((record) => unmarshall(record));
-        if (!params.resultSelector) {
-          console.log(items)
-          core.setOutput("output", items);
-        } else {
-          const itemList: string[] = [];
-          items.forEach(function (element) {
-              console.log(JSON.stringify(element));
-              Object.keys(element).forEach( (key: string) => { 
-                if ( key == params.resultSelector) {
-                  itemList.push(element[key]);
-                }
-              } )
-            });
-            console.log(itemList)
-            core.setOutput("output", itemList)
+(async() => {
+  try {
+    let result: any[];
+    const data = await ddbDocClient.send(new QueryCommand({
+      ...JSON.parse(params.input),
+      TableName: params.table,
+    }));
+    result = data.Items!.map((record) => unmarshall(record));
+    if (params.resultSelector) {
+      const itemList: string[] = [];
+      result.forEach(function (element) {
+        Object.keys(element).forEach((key: string) => {
+          if (key == params.resultSelector) {
+            itemList.push(element[key]);
           }
-        } catch (err) {
-            console.error(err);
-          }
-        };
-run();
+        })
+      });
+      result = itemList;
+    }
+    core.setOutput("output", JSON.stringify({ "result": result }))
+  } catch (err) {
+    console.error(err);
+  }
+})();
 
